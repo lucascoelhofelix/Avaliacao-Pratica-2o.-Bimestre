25f68d6b6a6b59b74e45c7bda73004ade17e38ec
lucascoelhofelix <167718769+lucascoelhofelix@users.noreply.github.com>
2025-11-09 23:07:20 -0300

    Update MainDemo.java

--- MainDemo.java
+++ MainDemo.java
@@ -14,31 +14,4 @@ public class MainDemo {
             complexContext,
             new VaRStrategy() // Estratégia inicial
         );
-
-        // --- Execução 1: Usando VaR ---
-        System.out.println("\n######################################");
-        System.out.println("## Execução 1: Value at Risk (VaR) ##");
-        System.out.println("######################################");
-        String result1 = processor.processRisk();
-        System.out.println("Resultado Final: " + result1);
-
-
-        // --- Execução 2: Troca Dinâmica para Expected Shortfall (ES) ---
-        // Troca de algoritmo em tempo de execução, sem mudar a lógica do 'processor'.
-        processor.setStrategy(new ExpectedShortfallStrategy());
-        System.out.println("\n#################################################");
-        System.out.println("## Execução 2: Expected Shortfall (Troca Din.) ##");
-        System.out.println("#################################################");
-        String result2 = processor.processRisk();
-        System.out.println("Resultado Final: " + result2);
-
-
-        // --- Execução 3: Troca Dinâmica para Stress Testing ---
-        processor.setStrategy(new StressTestingStrategy());
-        System.out.println("\n############################################");
-        System.out.println("## Execução 3: Stress Testing (Troca Din.) ##");
-        System.out.println("############################################");
-        String result3 = processor.processRisk();
-        System.out.println("Resultado Final: " + result3);
-    }
 }
